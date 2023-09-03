7c5c15bb7178a2cd46f49fe1cb88b541ec269c34
Haziqbatak <haziqimtiyaz17@gmail.com>
2023-09-03 20:47:02 +0700

    new file

---
+++ assets/js/image.js
@@ -0,0 +1,10 @@
+//Navbar Toogle Button
+
+const toogleButton = document.getElementsByClassName('toogle')[0];
+const navbarItem = document.getElementsByClassName('navbar-item');
+toogleButton.addEventListener('click', function() {
+    for(let i = 0; i < navbarItem.length; i++){
+        navbarItem[i].classList.toggle('active');
+}
+
+});
